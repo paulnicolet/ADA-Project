1fb5f92cb04e0c02adb5d623cd8679b09bb3f7a1
paulnicolet <nicoletpaul@orange.fr>
2016-12-18 17:22:14 +0100

    node generation fct & test

--- swiss_flows/node.py
+++ swiss_flows/node.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import pickle
+import warnings
 from haversine import haversine
 
 class Node:
@@ -22,12 +25,74 @@ class Node:
         """ Return the distance between two nodes in kilometers. """
         return haversine(self.position, other.position)
 
-    @staticmethod
-    def generate_nodes():
-        pass
-
     def __str__(self):
         return '[Node] {}, {}, {}, radius = {}.'.format(self.name,
                                                         self.canton,
                                                         self.position,
                                                         self.radius)
+
+    @staticmethod
+    def generate_nodes(n_nodes=10):
+        """
+        Generate the nodes
+        and save it to a file nodes_<n_nodes>.pkl in the data/ directory.
+        If the file already exists, simply return the list of nodes.
+        See nodes.ypnb notebook for the detailed process.
+
+        Parameters:
+            n_nodes Number of nodes to generate.
+        """
+        filepath = '../data/nodes_{}.pkl'.format(n_nodes)
+
+        # Look if the file already exists
+        try:
+            with open(filepath, 'rb') as file:
+                warnings.warn('File already exists, importing ...', UserWarning)
+                nodes = pickle.load(file)
+            return nodes
+        except FileNotFoundError:
+                pass
+
+        # Import data
+        df = pd.read_csv('../data/CH/CH.txt', header=None, encoding='utf8',
+                                              delimiter='\t', dtype={9: str})
+
+        # Build the index
+        index = ['geonameid', 'name', 'asciiname', 'alternatenames',
+                 'latitude', 'longitude', 'feature class', 'feature code',
+                 'country code', 'cc2', 'admin1 code', 'admin2 code',
+                 'admin3 code', 'admin4 code','population', 'elevation', 'dem',
+                 'timezone', 'modification date']
+
+        df.columns = index
+
+        # Drop null population, keep cities, drop useless columns
+        df = df[df['population'] != 0]
+        df = df[df['feature code'].str.contains(r'PPL(A\d?|C)?$')]
+        df = df[['asciiname', 'latitude', 'longitude', 'admin1 code',
+                'feature code','population']]
+
+        # Sort rows by population and take the n_nodes first
+        df = df.sort_values(by='population', ascending=False)
+        if df.shape[0] < n_nodes:
+            warnings.warn(
+                '{} nodes requested, {} nodes max, returns {} nodes'.format(n_nodes, df.shape[0], df.shape[0]),
+                UserWarning)
+
+        df = df[:n_nodes]
+
+        # Generate the list of node
+        nodes = []
+        for row in df.iterrows():
+            args = {'name': row[1].asciiname,
+                    'position': (row[1].latitude, row[1].longitude),
+                    'population': row[1].population,
+                    'canton': row[1]['admin1 code']}
+
+            nodes.append(Node(**args))
+
+        # Save the result
+        with open(filepath, 'wb') as file:
+            pickle.dump(nodes, file)
+
+        return nodes
