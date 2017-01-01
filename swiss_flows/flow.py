7e16068a0f37d82487f5324836752c5529a80f46
Lois Talagrand <lois.talagrand@epfl.ch>
2017-01-01 16:39:46 +0100

    Flow: Add time attributes

--- swiss_flows/flow.py
+++ swiss_flows/flow.py
@@ -5,19 +5,25 @@ class Flow:
     Parameters:
         src the source Node
         dst the destination Node
+        src_time the emission time at the source Node
+        dst_time the emission time at the destination Node
         directed True if the flow is directed
         weight importance of the flow
     """
     
-    def __init__(self, src, dst, directed=False):
+    def __init__(self, src, dst, src_time, dst_time, directed=False):
         self.src = src
         self.dst = dst
+        self.src_time = src_time
+        self.dst_time = dst_time
         self.directed = directed
         self.weight = 0
 
     def __str__(self):
         link = '-->' if self.directed else '<-->'
-        return '[Flow] {} {} {}, weight = {}.'.format(self.src.name,
-                                                        link,
-                                                        self.dst.name,
-                                                        self.weight)
+        return '[Flow] {} {} {} {} {}, weight = {}.'.format(self.src.name,
+                                                      link,
+                                                      self.dst.name,
+                                                      self.src_time,
+                                                      self.dst_time,
+                                                      self.weight)
