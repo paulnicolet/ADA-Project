448f64c5976d4fed1960255019c0b964bb33fb82
paulnicolet <nicoletpaul@orange.fr>
2017-01-15 17:22:55 +0100

    fix compatibility with actual dataset

--- swiss_flows/cleaning.py
+++ swiss_flows/cleaning.py
@@ -5,19 +5,24 @@ import os
 import csv
 import sys
 
-def clean_tweets(filename):
+def clean_tweets(file_path, tosave_path):
+	"""
+	Clean the tweet data set to keep only the desired features and save the result.
 
-	# Load the dirty tweets
-	# Taken from Slack: https://adaepfl.slack.com/archives/twitter/p1480527805000002
-	data_path = os.path.dirname('__file__') + '../data/twitter-swisscom/sample.tsv'
-	df = pd.read_csv(data_path, sep="\t",encoding='utf-8', escapechar='\\', quoting=csv.QUOTE_NONE, header=None, na_values='N')
+	Parameters:
+		file_path 	The path of the original file.
+		tosave_path Path of the new csv file to save.
 
+	Returns:
+		The dataframe of clean tweets.
+	"""
 	# Load the schema
 	schema_path = os.path.dirname('__file__') + '../data/twitter-swisscom/schema.txt'
 	schema = pd.read_csv(schema_path, delim_whitespace=True, header=None)
 
-	# Assign column names
-	df.columns = schema[1]
+	# Load the dirty tweets
+	data_path = os.path.dirname('__file__') + file_path
+	df = pd.read_csv(data_path, sep='\t',encoding='utf-8', escapechar='\\', quoting=csv.QUOTE_NONE, names=schema[1], na_values='N')
 
 	# Keep only the useful columns
 	useful_col = ['id', 'userId', 'createdAt', 'placeLongitude', 'placeLatitude']
@@ -28,19 +33,38 @@ def clean_tweets(filename):
 	df = df.dropna(subset=imp_col, how='any')
 
 	# Write in a file
-	df.to_csv(filename + '.csv', index=False)
+	df.to_csv(tosave_path + '.csv', index=False)
 
 	return df
 
-def filter_users(df):
+def filter_users(clean_tweets_path, save=False, tosave_path=None):
+	"""
+	Keep only users with more than one tweet and save them
+	as a dictionnary of the form {'user_id': [list of tweets]}
+
+	Parameters:
+		clean_tweets_path 	Path of the clean tweets to treat.
+		tosave_path 		Path of the .pkl to save.
+
+	Returns:
+		The dictionnary of tweets mapped to their user.
+	"""
+	# Load the clean tweets
+	df = pd.read_csv(clean_tweets_path)
+
+	# Group by user id
 	grouped = df.groupby('userId')
 
 	user_tweets = {}
 
+	# Filter users and update dictionnary
 	for user, tweets in grouped:
 		if tweets.shape[0] > 1:
 			user_tweets[user] = tweets.drop('userId', axis=1).values.tolist()
 
-	# Save the result
-	with open('../data/filtered_users.pkl', 'wb') as file:
-		pickle.dump(user_tweets, file)
+	if save:
+		# Save the result
+		with open(tosave_path + '.pkl', 'wb') as file:
+			pickle.dump(user_tweets, file)
+
+	return user_tweets
